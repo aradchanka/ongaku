d5e5f1d3b365dc42e87219eac74b5c27975ea09e
tomi <tomi@tomi>
2025-02-08 22:27:26 +0100

    Add ionic app

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'ongaku',
+  webDir: 'www'
+};
+
+export default config;
