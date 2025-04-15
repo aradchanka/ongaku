868a8c9241fd0139a9901697ac08946c3b968886
anthony.by.ateo <anthony.by.ateo@gmail.com>
2025-04-15 05:15:36 +0000

    Add list of cards in tab1

--- src/app/tab1/tab1.page.ts
+++ src/app/tab1/tab1.page.ts
@@ -5,9 +5,27 @@ import { Component } from '@angular/core';
   templateUrl: 'tab1.page.html',
   styleUrls: ['tab1.page.scss'],
   standalone: false,
-})
+}) 
 export class Tab1Page {
+  songs: { title: string; imageUrl: string }[] = [
+    {
+      title: 'Song 1',
+      imageUrl: 'https://ionicframework.com/docs/img/demos/card-media.png',
+    },
+    {
+      title: 'Song 2',
+      imageUrl: 'https://ionicframework.com/docs/img/demos/card-media.png',
+    },
+    {
+      title: 'Song 3',
+      imageUrl: 'https://ionicframework.com/docs/img/demos/card-media.png',
+    },
+    {
+      title: 'Song 4',
+      imageUrl: 'https://ionicframework.com/docs/img/demos/card-media.png',
+    },
+    { title: 'Song 5', imageUrl: 'https://ionicframework.com/docs/img/demos/card-media.png' },
+  ];
 
   constructor() {}
-
 }
